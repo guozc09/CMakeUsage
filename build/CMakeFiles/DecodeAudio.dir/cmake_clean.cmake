898cfccf4d637a65f7ba29ae899fdaf56373e335
Guo Zhc <guozc09@163.com>
2020-12-30 00:44:01 +0800

    CMake 的首次构建

---
+++ build/CMakeFiles/DecodeAudio.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DecodeAudio.dir/decoder.cpp.o"
+  "CMakeFiles/DecodeAudio.dir/test.cpp.o"
+  "bin/DecodeAudio"
+  "bin/DecodeAudio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DecodeAudio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/DecodeAudioConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for DecodeAudio
+#define DecodeAudio_VERSION_MAJOR 1
+#define DecodeAudio_VERSION_MINOR 0
