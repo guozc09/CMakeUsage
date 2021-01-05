67ae3b01ba7238b8537b6dd135a4c17d7dda1666
Guo Zhc <guozc09@163.com>
2021-01-05 10:09:06 +0800

    搭建VSCode gdb调试

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
     AudioDecoder decoder;
-    string fileName = "../../doc/那女孩对我说";
+    string fileName = "/home/zhcguo/linuxMount/AudioStreamPlayer/3rd_src/CMakeUsage/doc/那女孩对我说";
     string infile = fileName + ".mp3";
     string outfile = fileName + ".pcm";
     decoder.decodeFile(infile, outfile);
